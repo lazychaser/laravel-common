e76a824d9f9ff848a5508b554d6f5b24059b5a17
Alexander Kalnoy <lazychaser@gmail.com>
2014-06-02 17:50:41 +0400

    Add FormBuilder::appends

--- Kalnoy/LaravelCommon/Html/FormBuilder.php
+++ Kalnoy/LaravelCommon/Html/FormBuilder.php
@@ -45,4 +45,35 @@ class FormBuilder extends \Illuminate\Html\FormBuilder {
     {
         return parent::getIdAttribute($this->getIdFromName($name), $attributes);
     }
+
+    /**
+     * Append some hidden fields. Multilevel arrays are supported.
+     * 
+     * @param array $input
+     * @param string $key
+     * 
+     * @return string
+     */
+    public function appends(array $input, $key = '')
+    {
+        $html = '';
+
+        foreach ($input as $inner => $value)
+        {
+            if ($key === '_token') continue;
+            
+            if ($key) $inner = $key.'['.$inner.']';
+            
+            if (is_array($value))
+            {
+                $html .= $this->appends($value, $inner);
+
+                continue;
+            }
+
+            $html .= '<input type="hidden" name="'.$inner.'" value="'.$this->html->entities($value).'">'.PHP_EOL;
+        }
+
+        return $html;
+    }
 }
