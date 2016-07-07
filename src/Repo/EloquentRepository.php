38c71c1d9e9cf8d9baca1fba0a7c42cbeb7d41e9
Aleksander Kalnoy <lazychaser@gmail.com>
2016-07-07 15:10:45 +0300

    Added relation loading

--- src/Repo/EloquentRepository.php
+++ src/Repo/EloquentRepository.php
@@ -4,7 +4,13 @@ namespace Kalnoy\LaravelCommon\Repo;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Kalnoy\LaravelCommon\DataMapping\DataSources\Xml\Collection;
 
+/**
+ * Class EloquentRepository
+ *
+ * @package Kalnoy\LaravelCommon\Repo
+ */
 class EloquentRepository
 {
     /**
@@ -26,5 +32,22 @@ class EloquentRepository
     public function newQuery()
     {
         return $this->newModel()->newQuery();
+    }
+
+    /**
+     * @param Collection $items
+     * @param string $relation
+     * @param $value
+     *
+     * @return Collection
+     */
+    public function fillItemListRelation($items, $relation, $value)
+    {
+        /** @var Model $item */
+        foreach ($items as $item) {
+            $item->setRelation($relation, $value);
+        }
+
+        return $items;
     }
 }
